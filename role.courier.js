9d2070dd4912831601f298877dd5802e08b3c6c6
sindrekjr <sindre@sindrekjr.no>
2017-11-16 20:17:27 +0100

    role.courier ignores dropped resources less than 50, and goes for the ones that are closest

--- role.courier.js
+++ role.courier.js
@@ -16,30 +16,39 @@ var role = {
             });
             var targets = creep.room.find(FIND_STRUCTURES, {
                     filter: (structure) => {
-                        return (structure.structureType == STRUCTURE_STORAGE || structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN || structure.structureType == STRUCTURE_TOWER) &&
-                            (structure.energy < structure.energyCapacity);
+                        return (
+                            structure.structureType == STRUCTURE_STORAGE 
+                            || structure.structureType == STRUCTURE_EXTENSION 
+                            || structure.structureType == STRUCTURE_SPAWN 
+                            || structure.structureType == STRUCTURE_TOWER
+                            ) 
+                        && (structure.energy < structure.energyCapacity);
                     }
             });
-            if(towers.length) {
+            /*if(towers.length) {
                 if(creep.transfer(towers[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                     creep.moveTo(towers[0]);
                 }
-            } else if(targets.length) {
+            } else */if(targets.length) {
                 if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                     creep.moveTo(targets[0]);
                 }
             }
         } else {
             if(creep.carry.energy < creep.carryCapacity) {
-                var drops = creep.room.find(FIND_DROPPED_RESOURCES);
+                var drops = creep.pos.findClosestByPath(FIND_DROPPED_RESOURCES, {
+                    filter: (drop) => {
+                        return (drop.energy >= 50);
+                    }
+                });
                 var containers = creep.pos.findClosestByPath(FIND_STRUCTURES, {
                     filter: (structure) => {
                         return ((structure.structureType == STRUCTURE_CONTAINER) && (structure.store[RESOURCE_ENERGY] > 0))
                     }
                 });
-                if(drops.length) {
-                    if(creep.pickup(drops[0]) == ERR_NOT_IN_RANGE) {
-                        creep.moveTo(drops[0]);
+                if(drops) {
+                    if(creep.pickup(drops) == ERR_NOT_IN_RANGE) {
+                        creep.moveTo(drops);
                     }
                 } else if(containers) {
                     if(creep.withdraw(containers, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
